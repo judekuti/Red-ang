5046e046d06abf908547920296e01ff711dc7aac
Jude Kuti <judekuti@gmail.com>
2018-03-15 07:30:23 -0500

    Include a const Initial_State and an interface IAppState

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,5 +1,11 @@
 export interface IAppState{
+    todos: ITodo[];
+    lastUpdate: Date;
+}
 
+export const INITIAL_STATE: IAppState = {
+    todos: [],
+    lastUpdate: null
 }
 
 export function rootReducer(state, action){
