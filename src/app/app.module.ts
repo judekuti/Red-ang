2e0b26ba3436ceee378983b0e8b08c5abd3f89a0
Jude Kuti <judekuti@gmail.com>
2018-03-15 07:42:44 -0500

    Import all from store.ts to app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgRedux, NgReduxModule } from '@angular-redux/store'
+import { IAppState, rootReducer, INITIAL_STATE } from './store'
 
 
 import { AppComponent } from './app.component';
